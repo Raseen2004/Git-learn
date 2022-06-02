5ddb10b0919bab7550d6fe7cabd10a49f2c00e58
Raseen2004 <rajaraseen105@gmail.com>
2022-06-02 20:26:12 +0530

    First commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+  
+int main()
+{   
+    // declare and initialize string
+    char h[] = "Hello";
+    char g[] = "GitHub";
+      
+    // print string
+    printf("%s %s",h,g);
+      
+    return 0;
+}
